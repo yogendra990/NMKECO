d69158f0f3afc09d7d182b23210aa571b8e1e4cd
ASWAMEDHA\Yogendra Atluri <yogendraatluri99@gmail.com>
2017-10-09 15:33:50 -0700

    Commiting few things

--- nmk-ecomApp/nmkEcoAuth/nmkEcoLogin/nmkEcoLoginController/nmkEcoLogin.controller.js
+++ nmk-ecomApp/nmkEcoAuth/nmkEcoLogin/nmkEcoLoginController/nmkEcoLogin.controller.js
@@ -1,14 +1,25 @@
 (function(){
  'use strict';
  var nmkEcoApp = angular.module('nmkEco');
- nmkEcoApp.controller('nmkEcoLoginController',['$scope','nmkEcoAuthService',function($scope, nmkEcoAuthService){
+ nmkEcoApp.controller('nmkEcoLoginController',['$scope','nmkEcoAuthService','$timeout',function($scope, nmkEcoAuthService,$timeout){
     $scope.login ='Yogendra';
+    var vm = this;
     console.log($scope.login);
+    $scope.Validator;
+    $scope.PreloaderLogin;
     $scope.onLogin = function(form){
+        vm.EventData = {
+            'validator':true,
+            'PreloaderLogin':'../../../../shared/Preloaders/Preloader_6.gif'
+        };
+        $scope.$emit('LoginValidation',vm.EventData);        
     console.log(form);
     $scope.apikey="JF5eMlrB2pr3XkySDpW1h7bAJbIkUw0ayHWjgJjG";
     $scope.params = "&per_page=8";
     var onSuccess = function(response){
+     $timeout([function(){
+
+     }]);
     console.log(response);
     }
     var onError = function(response){
@@ -16,5 +27,10 @@
     }
      nmkEcoAuthService.ValidateUser($scope.apikey, $scope.params).then(onSuccess, onError);
     }
+
+    $scope.$on('LoginValidation',function(event,data){
+        $scope.Validator = data.validator;
+        $scope.PreloaderLogin = data.PreloaderLogin;
+    });
  }]);
 })();
